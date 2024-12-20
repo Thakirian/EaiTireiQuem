0296fec1ec91c0ad52fa201936c7bf38accdf8a7
Ana Beatriz <anabeatrizmferraz1@outlook.com>
2024-12-20 12:53:55 -0300

    feat: adicona editar dados de usuarios

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,12 +4,15 @@ const { auth, isAdmin } = require("../middleware/auth");
 const {
   criar,
   buscarTodos,
+  atualizarDadosUsuario,
   excluirUsuario,
 } = require("../controller/userController");
 
 router.post("/criar-usuario", criar);
 
 router.get("/listar-usuarios", auth, isAdmin, buscarTodos);
+
+router.patch("/atualizar", auth, atualizarDadosUsuario);
 
 router.delete("/deletar-usuario/:id", auth, isAdmin, excluirUsuario);
 
